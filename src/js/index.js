61caaaa70ea3bef069bcd1dfb67c81a06b07ea93
Dmitry Domnin <domnin.dm>
2023-09-05 15:53:12 +0300

    New styles added

--- src/js/index.js
+++ src/js/index.js
@@ -2,29 +2,30 @@ const archPic = document.querySelectorAll('.arch_pic'),
       archSlides = document.querySelectorAll('.arch_ex'),
       arrowContainer = document.querySelectorAll('.arrow_container'),
       archTape = document.querySelector('.arch_tape'),
-      sliderWindow = document.querySelector('.arch_examples_slider'),
-      width = window.getComputedStyle(sliderWindow).width;
+      sliderWindow = document.querySelector('.arch_examples_slider');
+
+let   width = window.getComputedStyle(sliderWindow).width;
 
 // Блок слайдера из раздела "Архитектура"
+setSlideSize();
 
-      let sliderIndex = 1,
+let sliderIndex = 1,
     offset = 0;
 
 archTape.style.width = archSlides.length * 100 + '%';
 archTape.style.transition = '0.5s all';
 
 function setSlideSize () {
+    width = window.getComputedStyle(sliderWindow).width;
     archSlides.forEach (element => {
         element.style.width = width;
     })
 }
 
-setSlideSize();
-
 arrowContainer.forEach(element => {
     
     element.addEventListener('click', (event) => {
-
+        
         if (event.target.classList.contains('left_arrow')) {
             if (offset == 0) {
                 offset = +width.slice(0, width.length - 2) * (archSlides.length - 1);
@@ -51,23 +52,22 @@ arrowContainer.forEach(element => {
 
 window.addEventListener('resize', (e) => {
     
-    setSlideSize();
-
-    if (window.matchMedia("(max-width: 768px)").matches) {
+    if (window.matchMedia('(min-width: 1px) and (max-width: 768px)').matches) {
+        setSlideSize();
         archPic.forEach((el) => {
             if (el.style !== 'width: 200px; height: 200px') {
-                el.style = 'width: 200px; height: 200px'
+                el.style = 'width: 180px; height: 180px'
             }
-    
         })
-      } else {
+    } else if (window.matchMedia('(min-width: 769px)').matches) {
+        setSlideSize();
         archPic.forEach((el) => {
             if (el.style !== 'width: 270px; height: 270px') {
                 el.style = 'width: 270px; height: 270px'
             }
             
         })
-      };  
+    }
 });
 
 // Блок работы табов раздела "Досуг"
